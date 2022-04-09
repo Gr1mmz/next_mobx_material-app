9664f31c307ebae8e9bce57621828c0779ef79dc
Gr1mmz <admin@kraevskiy-web.ru>
2022-04-09 19:22:25 +0800

    add spinner

--- components/TodoList/TodoList.tsx
+++ components/TodoList/TodoList.tsx
@@ -1,13 +1,16 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {observer} from "mobx-react-lite";
 import Todos from "../../store/todos";
 import TodoItem from "../TodoItem/TodoItem";
 import {Box} from "@mui/material";
 import {makeLocalStorage} from "../../store/makeLocalStorage";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const TodoList = observer(() => {
+    const [loading, setLoading] = useState(true);
     useEffect(() => {
         makeLocalStorage(Todos, "todosStore", ["todos"]);
+        setLoading(false);
     }, [])
     return (
         <Box sx={{
@@ -15,7 +18,14 @@ const TodoList = observer(() => {
             flexDirection: "column",
             gap: "1em"
         }}>
-            {Todos.getFilteredTodos(Todos.filter)?.map(todo => <TodoItem todo={todo} key={todo.id}/>)}
+            {loading
+                ? <ClipLoader
+                    color={"#2196f3"}
+                    loading={loading}
+                    size={50}
+                    css="margin: 0 auto"
+                />
+                :Todos.getFilteredTodos(Todos.filter)?.map(todo => <TodoItem todo={todo} key={todo.id}/>)}
         </Box>
     );
 })
